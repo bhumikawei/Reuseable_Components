97bdcdbe996c8789b01ff59c0e9427cd5f840cc0
Bhumika <bhumika04shimla@gmail.com>
2025-08-19 13:33:31 +0530

    did debugging

--- scripts/accordian.js
+++ scripts/accordian.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-    $(".accordion-toggle").click(function() {
-        //get attr value from clicked button
-        var dataToggleId = $(this).attr("data-accordion-toggle");
-        //hide all accordions
-        $(`.accordion-toggle[data-accordion-toggle != ${dataToggleId}], .accordion-tab[data-accordion-tab != ${dataToggleId}]`).removeClass("active"); 
+$(document).ready(function(){
+    $('.accordion-toggle').click(function(){
+        // Get attribute value from the clicked button
+        var dataToggleId = $(this).attr('data-accordion-toggle');
 
-        //add active class
-        $(`.accordion-toggle[data-accordion-toggle = ${dataToggleId}], .accordion-tab[data-accordion-tab = ${dataToggleId}]`).addClass("active"); 
+        // Remove active class fro mall elements which don't have the selected dataId
+        $(`.accordion-toggle[data-accordion-toggle!=${dataToggleId}], .accordion-tab[data-accordion-tab!=${dataToggleId}]`).removeClass('active');
+
+        // Add active class to the elements with matching dataId
+        $(`.accordion-toggle[data-accordion-toggle=${dataToggleId}], .accordion-tab[data-accordion-tab=${dataToggleId}]`).toggleClass('active');
+
     })
 })
